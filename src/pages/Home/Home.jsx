7eaf27940af1529e664eeabf08d989ff3da448ea
junaidapdev <junaidap.dev.com>
2022-06-27 11:29:40 +0530

    home page template added

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className="Home">
+
+        <div className="ProfileSide">post</div>
+        <div className="postSide">post</div>
+        <div className="rightSide">right</div>
+    </div>
+  )
+}
+
+export default Home

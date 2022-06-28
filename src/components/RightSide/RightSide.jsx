2bb6cd39739ab2ba887774d2d3a62685789ac5ec
junaidapdev <junaidap.dev.com>
2022-06-29 00:29:23 +0530

    Trend Card added

--- src/components/RightSide/RightSide.jsx
+++ src/components/RightSide/RightSide.jsx
@@ -4,6 +4,7 @@ import Home from "../../img/home.png";
 import Noti from "../../img/noti.png";
 import Comment from "../../img/comment.png";
 import { UilSetting } from "@iconscout/react-unicons";
+import TrendCard from "../TrendCard/TrendCard";
 
 const RightSide = () => {
   return (
@@ -14,7 +15,15 @@ const RightSide = () => {
         <img src={Noti} alt="" />
         <img src={Comment} alt="" />
       </div>
- </div>
+
+
+      <TrendCard />
+
+      <button className="button r-button">
+        Share
+      </button>
+
+    </div>
   );
 };
 
